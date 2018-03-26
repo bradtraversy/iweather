3e9355b8378b6e67bb6877ffd06fd7d8ff3d85a7
yovan72 <yovan7323@hotmail.com>
2018-03-26 15:23:21 +0400

    Geolocation added, weather forecasting details added with OpenweatherMap API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,84 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { Nav, Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { WeatherProvider } from './../providers/weather/weather';
+import { Storage } from '@ionic/storage';
+import { Events } from 'ionic-angular';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
-  rootPage:any = TabsPage;
+export class MyApp implements OnInit, AfterViewInit{
+  @ViewChild(Nav) nav: Nav;
+  rootPage:string = "HomePage";
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
+  pages: any[] = [];
+  location = {};
+  weather: any;
+  forecast: any;
+
+  hideDelete = false;
+  loc: string;
+  weatherUnits = [];
+
+  constructor(
+    public platform: Platform, 
+    public statusBar: StatusBar, 
+    public splashScreen: SplashScreen,
+    private weatherProvider: WeatherProvider,
+    private storage: Storage,
+    private events: Events,
+    private alertCtrl: AlertController
+  ) {
+    this.initializeApp();
+    }
+
+    ngOnInit() {
+      this.storage.forEach((value, key, index) => {
+        this.pages.push(JSON.parse(value));
+      });
+    }
+  
+    ngAfterViewInit() {
+      this.weatherProvider.getPosition().then(resp => {
+        this.weatherProvider.currentWeather(resp.coords.longitude, resp.coords.latitude)
+          .subscribe(res => {
+            if(res.length > 0){
+              this.weather = res[0];
+              this.loc = res[0].name;
+  
+              this.location = {
+                id: res[0].id,
+                icon: `http://openweathermap.org/img/w/${res[0].weather[0].icon}.png`,
+                current: res[0],
+                forecast: res[1]
+              }
+              this.storage.set(`location ${res[0].id}`, JSON.stringify(this.location));
+  
+              if(this.pages.length <= 0){
+                this.events.publish("cityinfo", this.location);
+              }
+              
+              this.nav.setRoot("HomePage", {"weatherInfo": this.location});
+            }
+          });
+      });
+  
+      this.events.subscribe("cityinfo", (data) => {
+        this.pages.push(data);
+      });
+    }
+    initializeApp() {
+      this.platform.ready().then(() => {
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        this.statusBar.styleDefault();
+        this.splashScreen.hide();
+      });
+    }
+
   }
-}
+
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,41 +1,129 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { WeatherProvider } from '../../providers/weather/weather';
+import { Component, ViewChild} from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 
+
+import moment from 'moment';
+
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
-  weather:any;
-  location:{
-    city:string,
-    state:string
-  }
+  @ViewChild('slideRef') slide: Slides;
+
+  locationInfo: any;
+  cityName: string;
+  slides = [];
+  bgImage: string;
 
   constructor(
     public navCtrl: NavController, 
-    private weatherProvider:WeatherProvider,
-    private storage:Storage) {
+    public navParams: NavParams,
+    private storage: Storage
+  ) { 
+    this.locationInfo = this.navParams.get("weatherInfo");
+    if(this.locationInfo) {
+      this.bgImage = this.BgImage(this.locationInfo.current.weather[0].main);
+    }
 
-  }
-
-  ionViewWillEnter(){
-    this.storage.get('location').then((val) => {
-      if(val != null){
-        this.location = JSON.parse(val);
-      } else {
-        this.location = {
-          city: 'Miami',
-          state: 'FL'
-        }
-      }
-
-      this.weatherProvider.getWeather(this.location.city, this.location.state)  .subscribe(weather => {
-          this.weather = weather.current_observation;
-        });
+    this.storage.forEach((value, key, index) => {
+      this.slides.push(JSON.parse(value));
     });
+
   }
 
+  ionViewDidLoad() {
+    this.locationInfo = this.navParams.get("weatherInfo");
+    if(this.locationInfo){
+      this.cityName = this.locationInfo.current.name;
+      this.slides.push(this.locationInfo);
+    }
+  }
+
+  slideChanged() {
+    let index = this.slide.getActiveIndex();
+    let name = this.slides[index].current.name;
+    this.cityName = name;
+
+    this.bgImage = this.BgImage(this.slides[index].current.weather[0].main);
+  }
+
+  GetDay = (time: number) => {
+      let day = new Date(time*1000).toISOString();
+      let d = new Date(day);
+      let weekday = [];
+      weekday[0] = "Sun";
+      weekday[1] = "Mon";
+      weekday[2] = "Tues";
+      weekday[3] = "Wed";
+      weekday[4] = "Thurs";
+      weekday[5] = "Fri";
+      weekday[6] = "Sat";
+
+      let n = weekday[d.getDay()];
+      return n;
+  }
+
+  GetDate = (time: number) => {
+      let day = moment(time*1000).format("DD/MM");
+      return day;
+  }
+
+  windDirection = (deg) => {
+      if (deg > 11.25 && deg < 33.75){
+        return "NNE";
+      }else if (deg > 33.75 && deg < 56.25){
+        return "ENE";
+      }else if (deg > 56.25 && deg < 78.75){
+        return "E";
+      }else if (deg > 78.75 && deg < 101.25){
+        return "ESE";
+      }else if (deg > 101.25 && deg < 123.75){
+        return "ESE";
+      }else if (deg > 123.75 && deg < 146.25){
+        return "SE";
+      }else if (deg > 146.25 && deg < 168.75){
+        return "SSE";
+      }else if (deg > 168.75 && deg < 191.25){
+        return "S";
+      }else if (deg > 191.25 && deg < 213.75){
+        return "SSW";
+      }else if (deg > 213.75 && deg < 236.25){
+        return "SW";
+      }else if (deg > 236.25 && deg < 258.75){
+        return "WSW";
+      }else if (deg > 258.75 && deg < 281.25){
+        return "W";
+      }else if (deg > 281.25 && deg < 303.75){
+        return "WNW";
+      }else if (deg > 303.75 && deg < 326.25){
+        return "NW";
+      }else if (deg > 326.25 && deg < 348.75){
+        return "NNW";
+      }else{
+        return "N"; 
+      }
+  }
+
+  BgImage = (val) => {
+    //let val = this.navParams.get("weatherInfo")
+    if(val == "Rain"){
+      return './assets/imgs/rain.jpg';
+    } else if(val == "Clear"){
+      return './assets/imgs/clear.jpg';
+    } else if(val == "Clouds"){
+      return './assets/imgs/clouds.jpg';
+    } else if(val == "Drizzle"){
+      return './assets/imgs/drizzle.jpg';
+    } else if(val == "Snow"){
+      return './assets/imgs/snow.jpg';
+    } else if(val == "ThunderStorm"){
+      return './assets/imgs/thunder.jpg';
+    } else {
+      return './assets/imgs/clear.jpg';
+    }
+  }
 }
